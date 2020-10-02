db453201f7fe4e8ba388fd000026829986258f69
Michael Cravotta <64832534+trashhouse@users.noreply.github.com>
2020-10-01 20:11:30 -0700

    whatever

--- DES427/Project1/P1-Functions.js
+++ DES427/Project1/P1-Functions.js
@@ -1,27 +1,38 @@
-let grLoop;
+/*let grIntro;
 let grPink;
 let grBlue;
 let title;
-let random;
+let ran;
+*/
 
-function preload() {
-    grLoop = createImg('grLoop.gif');
-    grPink = createImg('grPink.gif');
-    grBlue = createImg('grBlue.gif');   
+/*function preload() {
+    grIntro = loadImage('https://michaelcravotta.github.io/DES427/Project1/grLoop.gif');
+    grPink = loadImage('https://michaelcravotta.github.io/DES427/Project1/grPink.gif');
+    grBlue = loadImage('https://michaelcravotta.github.io/DES427/Project1/grBlue.gif');
+}*/
+
+function setup() {
+    createCanvas(1600, 1280);
+    background('yellow');
+    //image(grIntro, 0, 0);
 }
 
-function clickFW(){
-    grLoop.remove();
+function draw() {
+    
+}
+
+/*function mousePressed() {
+    if (mouseY > 980 &&  mouseY < 1120 && mouseX > 315 && mouseX < 490) {
+        clickFW;
+    }
+}
+function clickFW() {
+    grIntro.remove();
     title.remove();
-    if(random === 1) {
+    ran = random(1, 2);
+    if (ran === 1) {
         grPink.play();
     } else {
         grBlue.play();
     }
-}
-
-function setup(){
-    noCanvas();
-    title
-    
-}
+}*/
